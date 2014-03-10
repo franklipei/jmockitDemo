bf0b31e19afd80cc190154b416dca7875207035e
franklipei <frank.lipei@gmail.com>
2014-03-10 15:15:55 +0800

    add

--- src/main/java/SomeClass.java
+++ src/main/java/SomeClass.java
@@ -1,8 +1,33 @@
 public final class SomeClass
 {
+    SomeOtherClass other;
+
+//    public SomeOtherClass getOther() {
+//        return mock;
+//    }
+//
+//    public void setOther(SomeOtherClass mock) {
+//        this.mock = mock;
+//    }
+
+//    SomeClass(SomeOtherClass mock){
+//        this.mock = mock;
+//    }
+
+
+    int i;
+    int someIntegralProperty;
+
+
+    SomeClass(int someIntegralProperty1, int someIntegralProperty2){
+        this.someIntegralProperty = someIntegralProperty1;
+        this.someIntegralProperty = someIntegralProperty2;
+    }
+
 	public String doSomething(int i)
 	{
-		SomeOtherClass other = new SomeOtherClass("data");
+//		SomeOtherClass mock = new SomeOtherClass("data");
+        System.out.println("instance mock of SomeOtherClass is " + other.toString() + other.performSomeOperation(1));
 		return other.performSomeOperation(i);
 	}
 }
--- src/test/java/SomeClassTest.java
+++ src/test/java/SomeClassTest.java
@@ -6,37 +6,72 @@ import mockit.*;
 public final class SomeClassTest {
     @Tested
     SomeClass sut;
+
+    @Injectable
+    int someIntegralProperty2 = 2;
+
+    @Injectable
+    int someIntegralProperty = 1;
 //    @Injectable
 //    @NonStrict
-//    SomeOtherClass mock;
+//    SomeOtherClass mock2;//a mocked field
+
+    @Injectable
+    SomeOtherClass mock;
+
+    @Test
+    public void testSomething3() {
+
+        new NonStrictExpectations() {
+            {
+                mock.performSomeOperation(anyInt);
+                result = "mocked";
+            }
+        };
+
+        System.out.println("instance mock of SomeOtherClass is " + mock.toString() + mock.performSomeOperation(1));
+        System.out.println(sut.i + "/" + sut.someIntegralProperty);
+
+//        Deencapsulation.setField(sut, "mock", mock);
+
+        assertEquals("mocked", sut.doSomething(1234));
+    }
 
     @Test
     public void testSomething() {
         //Record phase
         new Expectations() {
-            @Mocked
-            SomeOtherClass mock;
+            //The use of @Mocked annotation is optional for mock parameters and for mock fields declared inside expectation blocks.
+//            @Injectable
+//            SomeOtherClass mock;
 
             {
                 new SomeOtherClass(anyString);
-                times = 1;
+                times = 0;
 
-                mock.performSomeOperation(anyInt);
-                result = "mocked";
+//                mock.performSomeOperation(anyInt);
+//                result = "mocked";
+
+//                mock2.performSomeOperation(anyInt);
+//                result = "mocked2";
+//                System.out.println("instance mock of SomeOtherClass is " + mock.toString() + ", " + mock.performSomeOperation(1));
+
             }
         };
+//        System.out.println("instance mock2 of SomeOtherClass is " + mock2.toString() + ", " + mock2.performSomeOperation(1));
+
 
         //Replay phase
         assertEquals("mocked", sut.doSomething(1234));
 
         //Verify phase
         //When one or more types get mocked non-strictly in a test, we often end up with a non-empty verification phase.
-        new Verifications() {
-            {
-                new SomeOtherClass(anyString);
-                times = 0;
-            }
-        };
+//        new Verifications() {
+//            {
+//                new SomeOtherClass(anyString);
+//                times = 0;
+//            }
+//        };
     }
 
     @Test
